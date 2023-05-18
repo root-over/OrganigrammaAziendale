389e954dc9a60b8a5e6cd0eed867ec46e560ab46
root_over <magliano_giuseppe@virgilio.it>
2023-05-18 18:40:18 +0200

    Cambiata interfaccia da lista a map

--- src/Dipendente.java
+++ src/Dipendente.java
@@ -1,52 +1,47 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Dipendente { //Rappresenta un dipendente e ha un riferimento al ruolo associato in un unità organizzativa specifica
     private String nome;
-    private List<UnitaOrganizzativa> unitaOrganizzativa;
-    private List<Ruolo> ruoli;
+    private Map<UnitaOrganizzativa,Ruolo> ruoli; //In questo caso ogni dipendente può avere un solo ruolo all'interno di un unità, se si vuole assegnargli più ruoli all'interno della stessa unità basta usare come valore una lista di ruoli
 
 
     public Dipendente(String nome){
         this.nome=nome;
-        this.unitaOrganizzativa=new ArrayList<>();
-        this.ruoli=new ArrayList<>();
+        this.ruoli=new HashMap<>();
     }
 
-    public void aggiungiAdUnita(UnitaOrganizzativa unita){
-        if (unitaOrganizzativa.contains(unita)){
-            System.out.println("Il dipendente fa già parte dell'unità "+unita);
+    public void aggiungiAdUnita(UnitaOrganizzativa unita,Ruolo ruolo){
+        if (unita.getRuoli().contains(ruolo)) {
+            if (ruoli.containsKey(unita)) {
+                System.out.println("Il dipendente fa già parte dell'unità " + unita);
+            } else {
+                this.ruoli.put(unita, ruolo);
+                System.out.println("Dipendente aggiunto all'unità " + unita + " con ruolo " + ruolo);
+            }
         }else {
-            this.unitaOrganizzativa.add(unita);
-            System.out.println("Dipendente aggiunto all'unità "+unita);
+            System.out.println("Il ruolo "+ruolo+" non è presente nell'unità "+unita);
         }
     }
 
-    public void aggiungiAdUnita(UnitaOrganizzativa unita, Ruolo ruolo){ //TODO
-        if (unitaOrganizzativa.contains(unita)){
-            System.out.println("Il dipendente fa già parte dell'unità "+unita);
+    public void rimuoviDaUnita(UnitaOrganizzativa unita){
+        if (!ruoli.containsKey(unita)){
+            System.out.println("Il dipendente non fa parte dell'unità "+unita);
         }else {
-            this.unitaOrganizzativa.add(unita);
-            System.out.println("Dipendente aggiunto all'unità "+unita);        }
-    }
-
-    public void rimuoviDaUnita(UnitaOrganizzativa unita){ //TODO RIMUOVERE ANCHE I RUOLI CHE AVEVA IN QUELLA UNITA
-        if (!unitaOrganizzativa.contains(unita)){
-            System.out.println("Il dipendente già non fa parte dell'unità "+unita);
-        }else {
-            this.unitaOrganizzativa.remove(unita);
+            this.ruoli.remove(unita);
             System.out.println("Dipendente rimosso dall'unità "+unita);
         }
     }
 
-    public void aggiungiRuolo(Ruolo ruolo, UnitaOrganizzativa unita) { //Serve specificre anche l'unità perchè è possibile che più unità hanno ruoli con lo stesso nome
-        if (!unitaOrganizzativa.contains(unita)) {
-            System.out.println("Il dipendente non fa parte dell'unità " + unita);
+    public void modificaRuolo(UnitaOrganizzativa unita, Ruolo ruolo){
+        if (!ruoli.containsKey(unita)){
+            System.out.println("Il Dipendente non fa parte dell'unità "+unita);
+        } else if (unita.getRuoli().contains(ruolo)) {
+            System.out.println("L'unità "+unita+" non ha il ruolo "+ruolo);
+        }else {
+            ruoli.put(unita,ruolo);
         }
-            else if (!unita.getRuoli().contains(ruolo)) {
-                System.out.println("Questa unità organizzativa non ha al suo interno il ruolo " + ruolo);
-            } else {
-                this.ruoli.add(ruolo);
-            }
     }
 }
