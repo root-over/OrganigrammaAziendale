e440a119f488198ef1f506188ee15999cc85076b
root_over <magliano_giuseppe@virgilio.it>
2023-05-18 18:54:59 +0200

    Sistemati alcuni problemi con i nomi

--- src/Dipendente.java
+++ src/Dipendente.java
@@ -16,32 +16,39 @@ public class Dipendente { //Rappresenta un dipendente e ha un riferimento al ruo
     public void aggiungiAdUnita(UnitaOrganizzativa unita,Ruolo ruolo){
         if (unita.getRuoli().contains(ruolo)) {
             if (ruoli.containsKey(unita)) {
-                System.out.println("Il dipendente fa già parte dell'unità " + unita);
+                System.out.println("Il dipendente fa già parte dell'unità " + unita.getNome());
             } else {
                 this.ruoli.put(unita, ruolo);
-                System.out.println("Dipendente aggiunto all'unità " + unita + " con ruolo " + ruolo);
+                System.out.println("Dipendente aggiunto all'unità " + unita.getNome() + " con ruolo " + ruolo.getNome());
             }
         }else {
-            System.out.println("Il ruolo "+ruolo+" non è presente nell'unità "+unita);
+            System.out.println("Il ruolo "+ruolo.getNome()+" non è presente nell'unità "+unita.getNome());
         }
     }
 
     public void rimuoviDaUnita(UnitaOrganizzativa unita){
         if (!ruoli.containsKey(unita)){
-            System.out.println("Il dipendente non fa parte dell'unità "+unita);
+            System.out.println("Il dipendente non fa parte dell'unità "+unita.getNome());
         }else {
             this.ruoli.remove(unita);
-            System.out.println("Dipendente rimosso dall'unità "+unita);
+            System.out.println("Dipendente rimosso dall'unità "+unita.getNome());
         }
     }
 
     public void modificaRuolo(UnitaOrganizzativa unita, Ruolo ruolo){
         if (!ruoli.containsKey(unita)){
-            System.out.println("Il Dipendente non fa parte dell'unità "+unita);
-        } else if (unita.getRuoli().contains(ruolo)) {
-            System.out.println("L'unità "+unita+" non ha il ruolo "+ruolo);
+            System.out.println("Il Dipendente non fa parte dell'unità "+unita.getNome());
+        }if (!unita.getRuoli().contains(ruolo)) {
+            System.out.println("L'unità "+unita.getNome()+" non ha il ruolo "+ruolo.getNome());
         }else {
             ruoli.put(unita,ruolo);
         }
+    }
+    public Map<UnitaOrganizzativa, Ruolo> getRuoli(){
+        return ruoli;
+    }
+
+    public String getNome(){
+        return nome;
     }
 }
